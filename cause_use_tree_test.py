be0d2a98b192a574aad9954dc15f7cc82be51a7e
jessica33tsai33 <57139009+jessica33tsai33@users.noreply.github.com>
2020-04-07 16:05:03 +0800

    creat creat_var_list function

--- cause_use_tree_test.py
+++ cause_use_tree_test.py
@@ -8,8 +8,40 @@ Created on Tue Mar 31 23:51:40 2020
 from ema_workbench.connectors import vensim
 from ema_workbench.connectors import vensimDLLwrapper as venDLL
 
+def creat_var_list(var):
+    """ 整理 get_varattrib 函式所產出的因子list """
+    varlist = []
+    for i in range(len(var)):
+        subvar = var[i]
+        if i == 0:
+            subvar = subvar[2:]
+        varlist.append(subvar)
+
+    if len(varlist[0]) == 0:
+        varlist = []
+        return varlist
+    else:
+        return varlist
+
+
 vensim.load_model(r"C://Users//Tsai Jessica//NTU//sdlab//SDGs_tools//class1_water1.vpm")
 
-a = venDLL.get_varattrib("outflow", 5)
+factor = "outflow"
+cause = 4
+use = 5
+resultList = []
 
-print(a)
+a = creat_var_list(venDLL.get_varattrib(factor, cause))
+resultList.append(a)
+
+print(len(a))
+
+stop = False
+i = 0
+
+while stop == False:
+    if len(a) == 0:
+        stop = True
+    else:
+        factor = a[i]
+        #resultList.append(creat_var_list(venDLL.get_varattrib(factor, cause)))
